b9f6cfe4460e33bebdf129a91e6e27469e30f99b
Gary Zou <jiarui_zou@outlook.com>
2026-04-01 15:22:49 +0800

    Add Lab 09 code

--- LAB Code/LAB/LAB - 09/Q1.cpp
+++ LAB Code/LAB/LAB - 09/Q1.cpp
@@ -6,3 +6,63 @@
 //
 
 #include <stdio.h>
+#include <iostream>
+using namespace std;
+class Complex {
+private:
+    double real;
+    double imag;
+
+public:
+    // Constructor
+    Complex(double r, double i) : real(r), imag(i) {}
+
+    // Overloading the + operator
+    // TODO: Write Your Code Here
+    
+    Complex operator+(const Complex& c1) const {
+        return Complex(real+c1.real, imag+c1.imag);
+    }
+
+    // Overloading the - operator
+    // TODO: Write Your Code Here
+    //
+    // ？这个黑粗体是怎么做到的？
+    Complex operator-(const Complex& c1) const { //use & to pass reference, instead of copying the object.
+        return Complex(real-c1.real, imag-c1.imag);
+    }
+    
+    // Overloading the << operator for output
+    friend std::ostream& operator<<(std::ostream& out, const Complex& c) {
+        // TODO: Write Your Code Here
+        //
+        //
+        //view the output stream as a flow~
+        
+        if(c.imag<0){
+            out << c.real << " - " << -c.imag << "i";
+        }else{
+            out << c.real << " + " << c.imag << "i";
+        }
+        return out;
+    }
+};
+int main() {
+    double d1, d2;
+
+    cout << "Enter first complex number:" << endl;
+    cin >> d1 >> d2;
+    Complex c1(d1, d2);
+
+    cout << "Enter second complex number:" << endl;
+    cin >> d1 >> d2;
+    Complex c2(d1, d2);
+
+    Complex sum = c1 + c2;
+    Complex diff = c1 - c2;
+
+    cout << "Sum: " << sum << endl;
+    cout << "Difference: " << diff << endl;
+
+    return 0;
+}
--- LAB Code/LAB/LAB - 09/Q2.cpp
+++ LAB Code/LAB/LAB - 09/Q2.cpp
@@ -6,3 +6,117 @@
 //
 
 #include <stdio.h>
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+class Product {
+private:
+    string name;
+    double price;
+
+public:
+    // Constructor
+    Product(string productName, double productPrice) {
+        //TO-DO: Write Your Code Here
+        //
+        //
+        if(productPrice > 0){
+            name = productName;
+            price = productPrice;
+        }else{
+            cout << "Error: Price cannot be negative!" << endl;
+        }
+        return;
+    }
+
+    // Destructor
+    //TO-DO: Write Your Code Here
+    //
+    //
+    ~Product() = default;
+
+    // Getters
+    //TO-DO: Write Your Code Here
+    //
+    //
+    string getName(){
+        return name;
+    }
+
+    double getPrice(){
+        return price;
+    }
+
+    // Setter for price (ensures non-negative value)
+    void setPrice(double newPrice) {
+        //TO-DO: Write Your Code Here
+        //
+        //
+        if(newPrice > 0){
+            price = newPrice;
+        }else{
+            cout << "Error: Price cannot be negative!" << endl;
+        }
+    }
+};
+int main() {
+    vector<Product> products;
+
+    cout << "1) Create new product" << endl;
+    cout << "2) Print Product" << endl;
+    cout << "3) Update Price" << endl;
+    cout << "4) Exit" << endl;
+
+    int cmd;
+    do {
+        cout << "Enter command: " << endl;
+        cin >> cmd;
+        switch(cmd) {
+        case 1:
+            {
+                cout << "Enter product name and price:" << endl;
+                string name;
+                double price;
+                cin >> name >> price;
+                products.emplace_back(Product(name, price));
+                cout << "Product created: " << name << " ($" << price << ")" << endl;
+            }
+            break;
+        case 2:
+            {
+                cout << "Enter product id:" << endl;
+                size_t index;
+                cin >> index;
+                if (index >= products.size()) {
+                    cout << "Selected index " << index << " is out of bounds" << endl;
+                    break;
+                }
+
+                cout << "The name of Product " << index << ": " << products[index].getName() << endl;
+                cout << "The price of Product " << index << ": $" << products[index].getPrice() << endl;
+            }
+            break;
+        case 3:
+            {
+                cout << "Enter product id and new price:" << endl;
+                size_t index;
+                double price;
+                cin >> index >> price;
+                if (index >= products.size()) {
+                    cout << "Selected index " << index << " is out of bounds" << endl;
+                    break;
+                }
+
+                products[index].setPrice(price);
+            }
+            break;
+        case 4:
+            break;
+        default:
+            cout << "Unknown cmd: " << cmd << endl;
+        }
+    } while (cmd != 4);
+
+    return 0;
+}
